350a596ddc12e49695c1d5e764a659d53df3763d
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2025-04-23 21:24:46 +0200

    feat: add morgan for request logging, update dependencies and format the code

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,40 @@
-import express, { Request, Response, Application, NextFunction } from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response, Application, NextFunction } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('This is automation pipeline');
+// Middleware for logging requests only in development node environment
+if (process.env.NODE_ENV !== "production") {
+  console.log(`Mode: ${process.env.NODE_ENV}`);
+  console.log("Logging enabled");
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is automation pipeline");
 });
 
 // Healthcheck endpoint
-app.get('/status', (req: Request, res: Response) => {
-    res.status(200).json(
-        {
-            status: "Ok",
-            timestamp: new Date().toISOString()
-        }
-    )
+app.get("/status", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "Ok",
+    timestamp: new Date().toISOString(),
+  });
 });
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-})
-
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at http://localhost:${port}`);
 });
